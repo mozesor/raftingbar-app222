da1e6b3c720e0a23886d2520e500699860f0b8fb
mozesor <167222442+mozesor@users.noreply.github.com>
2025-08-29 07:42:21 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ===== ClockWork config (editable) =====
-window.RB_API = "https://script.google.com/macros/s/AKfycbz1OM7zGXcyFk7nwC0ZK-8ph3K_gOqTeYH38Uc4QdMZ_lJ5lUd02HLv0D9oG90mqxHR/exec";
+window.RB_API = "https://script.google.com/macros/s/AKfycbzDoSgEMO0y-1YDto0BmSJsN0wFI53qIGyTWjX3vyzHqqO_7pZQxaK2fYm0N262J_5a/exec";
 
-// Optional: allow override via URL: ?api=https://...
+// Allow override via URL: ?api=https://...
 (function(){
   try{
     const sp = new URLSearchParams(location.search);
